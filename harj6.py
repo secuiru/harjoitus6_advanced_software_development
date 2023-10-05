626d6f5084be30c7c540472a85b5fb13485977fe
secuiru <vili.kauhanen@gmail.com>
2023-10-05 11:03:13 +0300

    third

--- harj6.py
+++ harj6.py
@@ -3,14 +3,17 @@ import winsound
 import time
 import threading
 from matplotlib.colors import ListedColormap
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from PIL import ImageTk, Image
 
 ikkuna=tk.Tk()
-ikkuna.title("Exercise 5")
+ikkuna.title("Exercise 6")
 ikkuna.geometry("700x700")
+
+
 
 monkey_img = Image.open("apina.png")
 monkey_img= monkey_img.resize((40,40))
@@ -33,6 +36,8 @@ saari.place(x=200,y=200)
 
 
 
+########################################### Allas
+
 allas=np.zeros((20,60))
 fig2,ax2=plt.subplots(figsize=(3,1))
 fig2.subplots_adjust(0,0,1,1,0,0)
@@ -41,7 +46,7 @@ for j in range(20):
      for i in range(60):
           allas[j,i]=1
 
-ax2.matshow(allas, cmap=ListedColormap(["gold", "blue"]),vmin=-2,vmax=3)
+ax2.matshow(allas, cmap=ListedColormap(["blue", "gold"]),vmin=-2,vmax=3)
 ax2.set_axis_off()
 fig2.set_facecolor("none")
 fig2.set_frameon("False")
@@ -49,16 +54,17 @@ erikois_kanvas2=FigureCanvasTkAgg(fig2,master=ikkuna)
 erikois_kanvas2.get_tk_widget().place(x=250,y=400)
 erikois_kanvas2.draw()
 
+########################################### ernestin oja
 
 ernestin_oja=np.zeros((100,1))
 
 for j in range(100):
      for i in range(1):
-         ernestin_oja[j,i]=0
+         ernestin_oja[j,i]=1
 
-fig1,ax1=plt.subplots(figsize=(0.2,2))
-fig1.subplots_adjust(0,0,1,10,0,0)
-ax1.matshow(ernestin_oja,cmap=ListedColormap(["gold", "blue"]),vmin=-2,vmax=3)
+fig1,ax1=plt.subplots(figsize=(0.02,2))
+fig1.subplots_adjust(0,0,1,1,0,0)
+ax1m = ax1.matshow(ernestin_oja,cmap=ListedColormap(["blue", "gold"]),vmin=-2,vmax=3)
 ax1.set_axis_off()
 fig1.set_facecolor("black")
 erikois_kanvas=FigureCanvasTkAgg(fig1,master=ikkuna)
@@ -66,15 +72,18 @@ erikois_kanvas.get_tk_widget().place(x=250,y=200)
 
 erikois_kanvas.draw()
 
+
+
+########################################### kernestin oja
 kernestin_oja=np.zeros((100,1))
 
 for j in range(100):
      for i in range(1):
-          kernestin_oja[j,i]=0
+          kernestin_oja[j,i]=1
 
-fig3,ax3=plt.subplots(figsize=(0.2,2))
-fig3.subplots_adjust(0,0,1,10,0,0)
-ax3.matshow(kernestin_oja,cmap=ListedColormap(["gold", "blue"]),vmin=-2,vmax=3)
+fig3,ax3=plt.subplots(figsize=(0.02,2))
+fig3.subplots_adjust(0,0,1,1,0,0)
+ax3m = ax3.matshow(kernestin_oja,cmap=ListedColormap(["blue", "gold"]),vmin=-2,vmax=3)
 ax3.set_axis_off()
 fig3.set_facecolor("black")
 erikois_kanvas3=FigureCanvasTkAgg(fig3,master=ikkuna)
@@ -99,38 +108,90 @@ def i_suppose_i_have_earned_so_much_points(amount_of_points):
         point_button[i].configure(bg='green')
         winsound.Beep(440+i*100,500)
 
-  
+##################################### ernesti
 
 def ernesti_hae_apina():
-    monkeypos=100
-    #apina_x=200
-    #apina_y=200
-    #apina = tk.Canvas(width=40,height=40)
-    #apina.create_image(apina_x,apina_y,image=monkey_img)
-    #monkey = tk.Label(ikkuna,image=monkey_img)
-    #monkey.place(x=monkeypos,y=200) 
+    monkeypos_x=200
+    monkeypos_y=400
+    count=0
     monkey = tk.Label(ikkuna,image=monkey_img)
-    while monkeypos < 400:
-        monkeypos=monkeypos+5
-        monkey.place(x=monkeypos,y=200)
-        time.sleep(0.3)    
+    fatigue=1
+    oja_pos=99
+    while monkeypos_y > 200:
+        
+        if ernestin_oja[oja_pos,0] == 1:
+            ernestin_oja[oja_pos,0] =ernestin_oja[oja_pos,0]-1
+
+            winsound.Beep(800,60)
+            time.sleep(fatigue)
+            fatigue = fatigue*2
+            ax1m.set_data(ernestin_oja)
+            #ax1.matshow(ernestin_oja)
+            fig1.canvas.draw()
+            fig1.canvas.flush_events()
+            
+        monkeypos_y=monkeypos_y-2
+        count = count +1
+        monkey.place(x=monkeypos_x,y=monkeypos_y)
+        oja_pos=oja_pos-1
+            
         print("liikkuu<")
+    print(count)
     i_suppose_i_have_earned_so_much_points(5)
+    
 
 def ernesti_hae_apina_thread():
     
     t=threading.Thread(target=ernesti_hae_apina)
     t.start()
 
-def movemint(self):
-     self
+#################################### kernesti
+
+def kernesti_hae_apina():
+    monkeypos_x=510
+    monkeypos_y=400
+    count=0
+    monkey = tk.Label(ikkuna,image=monkey_img)
+    fatigue=1
+    oja_pos=99
+    while monkeypos_y > 200:
+        
+        if kernestin_oja[oja_pos,0] == 1:
+            kernestin_oja[oja_pos,0] =kernestin_oja[oja_pos,0]-1
+            
+            winsound.Beep(800,60)
+            time.sleep(fatigue)
+            fatigue = fatigue*2
+            ax3m.set_data(kernestin_oja)
+            #ax1.matshow(ernestin_oja)
+            fig3.canvas.draw()
+            fig3.canvas.flush_events()
+            
+        monkeypos_y=monkeypos_y-2
+        count = count +1
+        monkey.place(x=monkeypos_x,y=monkeypos_y)
+        oja_pos=oja_pos-1
+            
+        print("liikkuu<")
+    print(count)
+    
+    
+
+def kernesti_hae_apina_thread():
+    
+    t=threading.Thread(target=kernesti_hae_apina)
+    t.start()
      
 
-#ernesti_hae_apina_thread()
 ernesti_hae_apina_thread() 
-ernesti_hae_apina_thread() 
+kernesti_hae_apina_thread()
 
-#while n==0:
+ernesti_button = tk.Button(ikkuna, text ="ernesti", command = ernesti_hae_apina_thread)
+ernesti_button.place(x=50,y=50)
+
+kernesti_button = tk.Button(ikkuna, text ="kernesti", command = kernesti_hae_apina_thread)
+kernesti_button.place(x=100,y=50)
+
 
 
     
